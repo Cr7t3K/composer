9d891de377460cd3901a49034f362dc612be4c21
Cr7t3K <49417277+Cr7t3K@users.noreply.github.com>
2020-04-08 19:39:45 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,7 +1,24 @@
 <?php
 require_once '../vendor/autoload.php';
 use App\Wcs\Hello;
+use HelloWorld\SayHello;
+
 
 $ok = new \App\Wcs\Hello();
 
 echo $ok->talk();
+
+$root = $_SERVER['DOCUMENT_ROOT'];
+
+$loader = new Twig\Loader\FilesystemLoader( __DIR__ . '/../src/views');
+$twig = new Twig\Environment($loader);
+
+
+$products = ['product1', 'product2', 'product3', 'product4', 'product5'];
+
+
+echo $twig->render('index.html.twig', ['products' => $products]);
+
+$package = new SayHello();
+
+echo $package->world();
